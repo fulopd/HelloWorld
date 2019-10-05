56f5542a59c094133d59496f87140277341719b6
Fülöp Dániel <fulopdaniel@gmail.com>
2019-10-05 11:26:16 +0200

    hello

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -4,11 +4,12 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Helló Vasvári");
-		
-		
-		System.out.println("Vezeték név: "+args[0]);
-		System.out.println("Kereszt név: "+args[1]);
+				
+		System.out.println("Vezeték név: "+ args[0]);
+		System.out.println("Kereszt név: "+ args[1]);
 		System.out.println("Életkor: " + args[2]);
+		
+		
 	}
 
 }
